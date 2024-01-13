098c77fb055c5b7363b1a30be9ce1e45c839813c
arun <arunyuvi0206@gmail.com>
2024-01-13 11:22:58 +0530

    database update

--- market.py
+++ market.py
@@ -2,6 +2,31 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+from flask_sqlalchemy import SQLAlchemy
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///marketsss.db'
+db = SQLAlchemy(app)
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f'Item {self.name}'
+
+
+    def __init__(self, name, price, barcode, description):
+        self.name = name
+        self.barcode =barcode
+        self.price = price
+        self.description =description
+        db.session.add(self)
+        db.session.commit()
+        return self
 
 @app.route("/")
 @app.route("/home")
@@ -11,10 +36,9 @@ def home_page():
 
 @app.route("/market")
 def market_page():
-    items = [
-        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
-        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
-
-    ]
+    items = Item.query.all()
     return render_template('market.html', items=items)
+
+
+if __name__ == '__main__':
+    app.run()
