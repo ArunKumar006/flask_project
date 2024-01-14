97d537b75179700f58d05b7ba79dc4a8c744f7bd
arun <arunyuvi0206@gmail.com>
2024-01-14 21:22:30 +0530

    update

--- market_project/market/forms.py
+++ market_project/market/forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
 
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField(label='user name', validators=[DataRequired()])
-    email_address = StringField(label='email address', validators=[DataRequired()])
-    password1 = PasswordField(label='password', validators=[DataRequired()])
-    password2 = PasswordField(label=' confirm password', validators=[DataRequired()])
+    username = StringField(label='user name', validators=[DataRequired(), Length(min=2, max=20)])
+    email_address = StringField(label='email address', validators=[DataRequired(), Email()])
+    password1 = PasswordField(label='password', validators=[DataRequired(),Length(min=6)])
+    password2 = PasswordField(label=' confirm password', validators=[DataRequired(), EqualTo('password1')])
     submit = SubmitField(label='create account')
--- market_project/market/routes.py
+++ market_project/market/routes.py
@@ -1,5 +1,5 @@
 from market import app
-from flask import render_template
+from flask import render_template,url_for,redirect,flash
 from market.models import Item
 from market.forms import RegistrationForm
 
@@ -16,7 +16,18 @@ def market_page():
     return render_template('market.html', items=items)
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register_page():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data,
+                              email=form.email.data,
+                              password_hash=form.password2.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+    if form.errors != {}:
+        for error in form.errors.values():
+            flash(f'Error: {error}')
+
     return render_template('register.html', form=form)
