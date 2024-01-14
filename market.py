1d8356cc2ac44db1cd2627efbcef007c5f9eca91
arun <arunyuvi0206@gmail.com>
2024-01-14 14:30:03 +0530

    added user model

--- market.py
+++ market.py
@@ -8,15 +8,30 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///marketsss.db'
 db = SQLAlchemy(app)
 
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.column(db.string(length=30), unique=True, nullable=False)
+    email_address = db.column(db.string(length=50), unique=True, nullable=False)
+    password_hash = db.column(db.string(length=60), nullable=False)
+    budget = db.column(db.Integer(), nullable=False, default=1000)
+    item = db.relationship('Item', backref='owned_user', lazy=True)
+
+    def __repr__(self):
+        return '<User{}>'.format(self.username)
+
+
+
+
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(length=30), nullable=False, unique=True)
     price = db.Column(db.Integer(), nullable=False)
     barcode = db.Column(db.String(length=12), nullable=False, unique=True)
     description = db.Column(db.String(length=1024), nullable=False, unique=True)
-
+    owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
     def __repr__(self):
         return f'Item {self.name}'
+
 
 @app.route("/")
 @app.route("/home")
