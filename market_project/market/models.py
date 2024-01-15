a907bcfca5d3ec0d99872052db3009efa2e8d1e1
arun <arunyuvi0206@gmail.com>
2024-01-15 14:00:33 +0530

    login flow completed

--- market_project/market/models.py
+++ market_project/market/models.py
@@ -1,8 +1,14 @@
-from market import db
+from market import db,login_manager
 from market import bcrypt
+from flask_login import UserMixin
 
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(length=30), unique=True, nullable=False)
     email_address = db.Column(db.String(length=50), unique=True, nullable=False)
@@ -18,11 +24,12 @@ class User(db.Model):
         return self.password_hash
 
     @password.setter
-    def password(self, plain_password):
-        self.password_hash = bcrypt.generate_password_hash(plain_password).decode('utf-8')
+    def password(self, plain_text_password):
+        self.password_hash = bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
 
-    def verify_password(self, plain_password):
-        return bcrypt.check_password_hash(self.password_hash, plain_password)
+    def check_password_correction(self, attempted_password):
+        print(self.password_hash, "password", attempted_password)
+        return bcrypt.check_password_hash(self.password_hash, attempted_password)
 
 
 class Item(db.Model):
--- market_project/market/routes.py
+++ market_project/market/routes.py
@@ -4,6 +4,7 @@ from .models import Item
 from .forms import RegistrationForm, LoginForm
 from .models import User
 from market import db
+from flask_login import login_user, logout_user
 @app.route('/')
 @app.route('/home')
 def home():
@@ -22,7 +23,7 @@ def register_page():
     if form.validate_on_submit():
         user_to_create = User(username=form.username.data,
                               email_address=form.email_address.data,
-                              password_hash=form.password2.data)
+                              password=form.password2.data)
         db.session.add(user_to_create)
         db.session.commit()
         return redirect(url_for('market_page'))
@@ -35,4 +36,15 @@ def register_page():
 @app.route('/login', methods=['GET', 'POST'])
 def login_page():
     form = LoginForm()
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(
+                attempted_password=form.password.data
+        ):
+            login_user(attempted_user)
+            flash(f'Success! You are logged in as: {attempted_user.username}', category='success')
+            return redirect(url_for('market_page'))
+        else:
+            flash('Username and password are not match! Please try again', category='danger')
+
     return render_template('login.html', form=form)
